e35f3bf0855fb2ea7266d7d0ac19588115d4c742
Sichao Quan <133809053+quansi0817@users.noreply.github.com>
2026-03-03 16:29:56 -0500

    Refactor SQL query to improve formatting
    Formatted SQL query for better readability.

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,4 +1,10 @@
+
 # Write your MySQL query statement below
-# Write your MySQL query statement below
-select p.firstName,p.lastName, a.city, a.state from Person p Left Join
-Address a on p.personId = a.personId
+SELECT
+  p.firstName,
+  p.lastName, 
+  a.city, 
+  a.state 
+FROM Person p 
+LEFT JOIN Address a 
+ON p.personId = a.personId
